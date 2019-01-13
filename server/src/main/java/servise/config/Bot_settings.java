4e89383496b0cae623f36fc307fb76f877ab36b7
narus <narus.od@gmail.com>
2019-01-13 13:39:29 +0200

    update webpack

--- server/src/main/java/servise/config/Bot_settings.java
+++ server/src/main/java/servise/config/Bot_settings.java
@@ -10,8 +10,8 @@ public class Bot_settings {
 
     public static  ArrayList<Bot_setting> getList() {
         ArrayList<Bot_setting> list = new ArrayList<>();
-        list.add(new Bot_setting("Exmo", ExmoExchange.class, "K-bb98c6cec34b24dc2bdd7250b2541d2a4fbbee6e", "S-d4411e7f4ba1af42900f5f05f0091ee5dbb16321") );
-        list.add(new Bot_setting("Binance" ,BinanceExchange.class, "594fb900-05d1-4029-8b0d-bd87bff333db", "8cZRt3XFMGJpyTME") );
+        list.add(new Bot_setting("Exmo", ExmoExchange.class, "K-bb99c6cec34b24dc2bdd7250b2541d2a4fbbee6e", "S-d4411e7f4ba1af42900f5f05f0091ee5dbb16321") );
+        list.add(new Bot_setting("Binance" ,BinanceExchange.class, "599fb900-05d1-4029-8b0d-bd87bff333db", "8cZRt3XFMGJpyTME") );
         return list;
     }
 
